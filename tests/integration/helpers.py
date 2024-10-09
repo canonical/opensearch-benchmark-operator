a685d974c464db84c6835f347b97db1472d72c8d
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2024-10-09 19:14:14 +0200

    Add first batch of integration tests

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import uuid
+import logging
 from pathlib import Path
 
 import pytest
@@ -9,106 +9,49 @@ import yaml
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
-MYSQL_APP_NAME = "mysql"
-PGSQL_APP_NAME = "postgresql"
 DURATION = 10
-K8S_DB_MODEL_NAME = "database-" + str(uuid.uuid4())[0:5]
-MICROK8S_CLOUD_NAME = "cloudk8s"
+TLS_CERTIFICATES_APP_NAME = "self-signed-certificates"
+SERIES = "jammy"
+UNIT_IDS = [0, 1, 2]
+IDLE_PERIOD = 75
 
 
-DB_CHARM = {
-    "mysql": {
-        "charm": "mysql",
-        "channel": "8.0/edge",
-        "config": {"profile": "testing"},
-        "app_name": MYSQL_APP_NAME,
-    },
-    "postgresql": {
-        "charm": "postgresql",
-        "channel": "14/edge",
-        "config": {"profile": "testing"},
-        "app_name": PGSQL_APP_NAME,
-    },
-    "mysql-k8s": {
-        "charm": "mysql-k8s",
-        "channel": "8.0/edge",
-        "config": {"profile": "testing"},
-        "app_name": MYSQL_APP_NAME,
-    },
-    "postgresql-k8s": {
-        "charm": "postgresql-k8s",
-        "channel": "14/edge",
-        "config": {"profile": "testing"},
-        "app_name": PGSQL_APP_NAME,
-    },
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s", datefmt="%H:%M:%S"
+)
+logger = logging.getLogger(__name__)
+
+
+MODEL_CONFIG = {
+    "logging-config": "<root>=INFO;unit=DEBUG",
+    "update-status-hook-interval": "5m",
+    "cloudinit-userdata": """postruncmd:
+        - [ 'sysctl', '-w', 'vm.max_map_count=262144' ]
+        - [ 'sysctl', '-w', 'fs.file-max=1048576' ]
+        - [ 'sysctl', '-w', 'vm.swappiness=0' ]
+        - [ 'sysctl', '-w', 'net.ipv4.tcp_retries2=5' ]
+    """,
 }
 
 
-DB_ROUTER = {
-    "mysql": {
-        "charm": "mysql-router",
-        "channel": "dpe/edge",
-        "config": {},
-        "app_name": "mysql-router",
-    },
-    "postgresql": {
-        "charm": "pgbouncer",
-        "channel": "1/edge",
-        "config": {},
-        "app_name": "pgbouncer",
-    },
-    "mysql-k8s": {
-        "charm": "mysql-router-k8s",
-        "channel": "8.0/edge",
-        "config": {},
-        "app_name": "mysql-router",
-    },
-    "postgresql-k8s": {
-        "charm": "pgbouncer-k8s",
-        "channel": "1/edge",
-        "config": {},
-        "app_name": "pgbouncer",
-    },
+ALL_GROUPS = {
+    deploy_type: pytest.param(
+        deploy_type,
+        id=deploy_type,
+        marks=[
+            pytest.mark.group(deploy_type),
+            pytest.mark.runner([
+                "self-hosted",
+                "linux",
+                "X64",
+                "jammy",
+                "xlarge" if deploy_type == "large" else "large",
+            ]),
+        ],
+    )
+    for deploy_type in ["large_deployment", "small_deployment"]
 }
 
-
-DEPLOY_ALL_GROUP_MARKS = [
-    (
-        pytest.param(
-            app,
-            router,
-            id=f"{app}_router-{router}",
-            marks=pytest.mark.group(f"{app}_router-{router}"),
-        )
-    )
-    for app in ["mysql", "postgresql", "mysql-k8s", "postgresql-k8s"]
-    for router in ([True, False] if not app.endswith("-k8s") else [True])
-]
-
-
-DEPLOY_VM_ONLY_GROUP_MARKS = [
-    (
-        pytest.param(
-            app,
-            router,
-            id=f"{app}_router-{router}",
-            marks=pytest.mark.group(f"{app}_router-{router}"),
-        )
-    )
-    for app in ["mysql", "postgresql"]
-    for router in [True, False]
-]
-
-
-DEPLOY_K8S_ONLY_GROUP_MARKS = [
-    (
-        pytest.param(
-            app,
-            router,
-            id=f"{app}_router-{router}",
-            marks=pytest.mark.group(f"{app}_router-{router}"),
-        )
-    )
-    for app in ["mysql-k8s", "postgresql-k8s"]
-    for router in [True]
-]
+ALL_DEPLOYMENTS = list(ALL_GROUPS.values())
+SMALL_DEPLOYMENTS = [ALL_GROUPS["small_deployment"]]
+LARGE_DEPLOYMENTS = [ALL_GROUPS["large_deployment"]]
